b6762c88a56e7b45d6550a2a3d79608921a851fd
Ben Summers <bsumm6@gmail.com>
2022-02-21 19:00:50 -0500

    added basic order form and attempt at both front and back end calls to save to user_metadata

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 //app.js
 let auth0 = null;
+let auth_user = null;
 
 
 const fetchAuthConfig = () => fetch("/auth_config.json");
@@ -11,15 +12,23 @@ const configureClient = async () => {
   auth0 = await createAuth0Client({
     domain: config.domain,
     client_id: config.clientId,
-    audience: config.audience 
+    audience: config.audience
+  });
+
+  auth_user = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+    audience: "https://dev-9r54t9mj.us.auth0.com/api/v2/",
+    scope: "openid profile email read:current_user update:current_user_metadata"
   });
 };
 
 
+
 window.onload = async () => {
   await configureClient();
-
   updateUI();
+  createEventListener();
 
  const isAuthenticated = await auth0.isAuthenticated();
 
@@ -51,7 +60,8 @@ const updateUI = async () => {
 
   // NEW - enable the button to call the API
   document.getElementById("btn-call-api").disabled = !isAuthenticated;
-
+  document.getElementById("btn-call-user-api").disabled = !isAuthenticated;
+  //document.getElementById("btn-call-get-admin-token").disabled = !isAuthenticated;
   
   // NEW - add logic to show/hide gated content after authentication
   if (isAuthenticated) {
@@ -62,7 +72,7 @@ const updateUI = async () => {
     ).innerHTML = await auth0.getTokenSilently();
 
     document.getElementById("ipt-user-profile").textContent = JSON.stringify(
-      await auth0.getUser()
+      await auth0.getUserinfo
     );
 
   } else {
@@ -109,4 +119,121 @@ const callApi = async () => {
     // Display errors in the console
     console.error(e);
   }
+};
+
+
+//submit order using custom API
+const SubmitOrderBackEnd = async (orderData) => {
+  try {
+
+    alert(orderData);
+    orderData = { "user_metadata" : orderData};
+
+    try {
+
+    // Get the access token from the Auth0 client
+    const token = await auth0.getTokenSilently();
+
+    const response = await fetch("/api/UpdateOrderHistory", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json; charset=UTF-8" 
+        },
+      body: JSON.stringify(orderData)
+    })
+    // .then(response => response.json())
+    // .then(json => console.log(json));
+
+    // Fetch the JSON result
+    const responseData = await response;
+    const responseJson = await responseData.json();
+    console.log(responseJson);
+
+
+    } catch (e) {
+        // Display errors in the console
+        console.error(e);
+      }
+
+  } catch (e) {
+    // Display errors in the console
+    alert(e.message);
+    console.error(e);
+  }
+
+};
+
+
+//update order history using auth managmenet api to add order to current_user metadata
+const SubmitOrder = async (orderData) => {
+  try {
+
+    // append user_metadata parent JSON field
+    orderData = { "user_metadata" : orderData};
+    console.log(orderData);
+    // Get the access token from the Auth0 client
+   const token = await auth_user.getTokenWithPopup();
+   
+
+    // call auth0 user management API
+    const response = await fetch("https://dev-9r54t9mj.us.auth0.com/api/v2/users/auth0%7C620ca8160e408c006ab39806", {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(orderData)
+    });
+    
+    if(!response.ok){
+      alert("Not ok reponse");
+    }
+    const responseData = await response.json();
+    console.log(responseData);
+
+    const response2 = await fetch("https://dev-9r54t9mj.us.auth0.com/api/v2/users/auth0%7C620ca8160e408c006ab39806", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if(!response2.ok){
+      alert("Not ok reponse");
+    }
+    const responseData2 = await response2.json();
+    console.log(responseData2);
+
+
+  } catch (e) {
+    // Display errors in the console
+    alert(e.message);
+    console.error(e);
+  }
+
+};
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const value = Object.fromEntries(data.entries());
+
+    //Submit to front end api call 
+    //SubmitOrder(value);
+
+    //Submit to backend 
+    SubmitOrderBackEnd(value);
+
+    //console.log({ value });
+  };
+
+
+function createEventListener(){
+
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', handleSubmit);
 };
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const { auth } = require("express-oauth2-jwt-bearer");
 const { join } = require("path");
 const authConfig = require("./auth_config.json");
+const axios = require("axios").default;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let AdminToken = "";
+//test token from API Test Explorer
+let TestAdminToken="PASTE_TEST_TOKEN_HERE"
 
 // create the JWT middleware
 
@@ -12,6 +21,80 @@ const checkJwt = auth({
   audience: authConfig.audience,
   issuerBaseURL: `https://${authConfig.domain}`
 });
+
+var AuthenticationClient = require('auth0').AuthenticationClient;
+
+var auth0 = new AuthenticationClient({
+domain: 'dev-9r54t9mj.us.auth0.com',
+  clientId: 'eobn8NS1cGR1rnQH2CyJbklfObOZm6bz',
+  clientSecret: 'U_U-oPp8UGGEsQN8K2bDe6FZRH2BXxXDDo3J1GRQpn2vdxD5T6ijeeuEJnhNwKcN',
+  scope: 'update:current_user_metadata'
+});
+
+auth0.clientCredentialsGrant(
+  {
+    audience: 'https://dev-9r54t9mj.us.auth0.com/api/v2/'
+  },
+  function (err, response) {
+    if (err) {
+      console.log(err);
+    }else{
+      AdminToken = response.access_token;
+      //console.log(response.access_token);
+  }
+  }
+);
+
+
+
+app.post("/api/UpdateOrderHistory", checkJwt, (req, res) => {
+
+
+  var order = req.body;
+  //console.log(AdminToken);
+  var options = {
+      method: 'PATCH',
+      url: 'https://dev-9r54t9mj.us.auth0.com/api/v2/users/auth0%7C620ca8160e408c006ab39806',
+        headers: {authorization: `Bearer ${TestAdminToken}`, 'content-type': 'application/json'},
+            data: order
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      res.send({
+        msg: response.data
+      });
+    }).catch(function (error) {
+      console.error(error);
+    });
+});
+
+
+app.get("/api/getAdminToken", (req, res) => {
+  var options = {
+    method: 'POST',
+    url: 'https://dev-9r54t9mj.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    body: '{"client_id":"yAvctZfZbm2WuKW9WuQQwAdGCp7gSNZk","client_secret":"cX7R_phG5QZ3_4jfvLusmcdXXWEUmi6-g6nYQZbF0W4mEwif7nVjeRJN_8ISWULg","audience":"https://dev-9r54t9mj.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
+    };
+
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      res.send({
+        msg: "Admin token data: " + response.data
+      });
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+});
+
+
+
+
+
 
 
 //Create an endpoint that uses the above middleware to
@@ -46,11 +129,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-// Listen on port 3000
-console.log(process.env.PORT)
-if(process.env.PORT){
-  app.listen(process.env.PORT, () => console.log("Application running on port process.env.PORT"));
-}
-else{
-  app.listen(3000, () => console.log("Application running on port 3000"));
-}
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Application running on port " + port))
+
